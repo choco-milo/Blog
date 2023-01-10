1e01c2b51d12d00aad147ee54184a10861d38f7a
choco-milo <owusujanice799@gmail.com>
2023-01-10 11:51:37 +0000

    code cleaner

--- main.py
+++ main.py
@@ -13,14 +13,12 @@ def home():
 
 @app.route('/post/<blog_id>')
 def blog(blog_id):
-    print(blog_id)
     response = requests.get('https://api.npoint.io/c3f99fbb923b8cf304f8')
     all_post = response.json()
     spec_item = 0
     for item in all_post:
         if item['id'] == int(blog_id):
             spec_item = item
-    print(spec_item)
     return render_template("post.html", blog_info=spec_item)
 
 
